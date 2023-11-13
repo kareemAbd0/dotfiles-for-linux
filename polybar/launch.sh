815daf396994f0ed42441877dce35106d143508f
kareemAbd0 <kareemabdo.7781@yahoo.com>
2023-11-13 17:42:23 +0200

    updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -14,3 +14,9 @@ echo "Bars launched..."
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar eDP1 &
 polybar HDMI1 &
+
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
